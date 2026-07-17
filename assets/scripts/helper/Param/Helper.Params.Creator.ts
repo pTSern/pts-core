b8e7f575d72de50fc745412e1405d872f682db9d
pTSern <tsern.dev@gmail.com>
2026-07-17 18:08:32 +0700

    push

--- assets/scripts/helper/Param/Helper.Params.Creator.ts
+++ assets/scripts/helper/Param/Helper.Params.Creator.ts
@@ -1,10 +1,36 @@
-import { _decorator, CCClass, Enum, js } from 'cc';
+import { _decorator, CCClass, CCInteger, Component, Node, Enum, js, v3, Vec3 } from 'cc';
 import { Helper_Param_Base } from './Helper.Param.Base';
 import { pClass, pConst } from '../../utils';
 import { Editor_Smart_SelfFocus } from '../../editor/Smart/Editor.Smart.SelfFocus';
 import { CC_EnumList } from '../../interfaces/cc/CC.IEnumable';
 
 const { ccclass, property } = _decorator;
+
+@ccclass("Y")
+class Y {
+    @property({ type: Vec3 })
+    v: Vec3 = null
+    @property({  })
+    z: Vec3 = v3()
+
+    @property({ type: CCInteger })
+    int: number = 10
+
+    @property({  })
+    str: string = 'mess'
+}
+
+@ccclass("X")
+class X {
+    @property({ type: Component })
+    comp: Component = null
+
+    @property({ type: Node })
+    xnod: Node = null
+
+    @property(Y)
+    y: Y = new Y()
+}
 
 @ccclass('Helper_Param_Creator')
 export class Helper_Param_Creator extends Editor_Smart_SelfFocus {
@@ -21,13 +47,35 @@ export class Helper_Param_Creator extends Editor_Smart_SelfFocus {
         this.focus();
     }
 
+    @property({  })
+    protected _type: string = ''
     @property({ type: Enum({}) })
-    type: string = ''
+    get type() { return this._type }
+    set type(x) {
+        this._type = x;
+        this.focus();
+    }
 
+    @property({  })
+    data: any = null
+
+    @property({ type: Component })
+    comp: Component = null
+
+    @property({ type: X })
+    x: X = new X()
 
     focus(): void {
+        console.log(">>> THAU", CCClass.Attr.getClassAttrs(Helper_Param_Creator));
         const _list = pClass.getAllCCClasses(this._filter);
         const _cce = CC_EnumList(Array.from(_list));
         CCClass.Attr.setClassAttr(this, 'type', 'enumList', _cce);
+
+        const _class = js.getClassByName(this._type);
+        if(!_class) return
+
+        CCClass.Attr.setClassAttr(this, 'data', 'type', 'Object')
+        CCClass.Attr.setClassAttr(this, 'data', 'ctor', _class)
+        CCClass.Attr.setClassAttr(this, 'data', 'default', () => new _class())
     }
 }
