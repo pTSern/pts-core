b0579b55b64b79fff3dae2cb3cfba5a6b5cc1188
pTSern <tsern.dev@gmail.com>
2026-07-17 14:15:48 +0700

    push

---
+++ assets/scripts/helper/Param/Helper.Param.Base.ts
@@ -0,0 +1,12 @@
+import { _decorator, Component } from 'cc';
+import { Editor_Smart_SelfFocus } from '../../editor/Smart/Editor.Smart.SelfFocus';
+
+const { ccclass, property } = _decorator;
+
+@ccclass('Helper_Param_Base')
+export abstract class Helper_Param_Base extends Editor_Smart_SelfFocus {
+    protected _types
+
+    focus(): void {
+    }
+}
